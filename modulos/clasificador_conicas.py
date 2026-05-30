b586c2966ae096a4bac3b17c145aa2397e8c2003
sofianoguera <sofiarodrigueznog@gmail.com>
2026-05-29 20:19:09 -0400

    fix: corrige clasificador_conicas para usar coeficiente B en lugar de C

--- modulos/clasificador_conicas.py
+++ modulos/clasificador_conicas.py
@@ -2,43 +2,42 @@ class ClasificadorDeConicas:
     def __init__(self, coeficientes):
         self.coeficientes = coeficientes
         self.coeficiente_a = coeficientes.get('A')
-        self.coeficiente_c = coeficientes.get('C')
-        
-        if self.coeficiente_a is None or self.coeficiente_c is None:
-            raise ValueError("El diccionario proporcionado no contiene los coeficientes 'A' y 'C'.")
+        self.coeficiente_b = coeficientes.get('B')
+
+        if self.coeficiente_a is None or self.coeficiente_b is None:
+            raise ValueError("El diccionario proporcionado no contiene los coeficientes 'A' y 'B'.")
 
     def comprobar_caso_degenerado(self):
-        if self.coeficiente_a == 0 and self.coeficiente_c == 0:
+        if self.coeficiente_a == 0 and self.coeficiente_b == 0:
             raise ValueError("No existen los terminos cuadraticos: la ecuacion no representa una conica.")
 
     def determinar_clasificacion_y_justificacion(self):
-        if self.coeficiente_a == self.coeficiente_c:
+        if self.coeficiente_a == self.coeficiente_b:
             nombreConica = "Circunferencia"
-            justificacion = "A (" + str(self.coeficiente_a) + ") es igual a C (" + str(self.coeficiente_c) + ") y ambos son distintos de cero."
-            
-        elif self.coeficiente_a == 0 or self.coeficiente_c == 0:
+            justificacion = "A (" + str(self.coeficiente_a) + ") es igual a B (" + str(self.coeficiente_b) + ") y ambos son distintos de cero."
+
+        elif self.coeficiente_a == 0 or self.coeficiente_b == 0:
             nombreConica = "Parabola"
-            justificacion = "Exactamente uno de los coeficientes principales es cero (A = " + str(self.coeficiente_a) + ", C = " + str(self.coeficiente_c) + ")."
-            
-        elif (self.coeficiente_a > 0 and self.coeficiente_c > 0) or (self.coeficiente_a < 0 and self.coeficiente_c < 0):
+            justificacion = "Exactamente uno de los coeficientes principales es cero (A = " + str(self.coeficiente_a) + ", B = " + str(self.coeficiente_b) + ")."
+
+        elif (self.coeficiente_a > 0 and self.coeficiente_b > 0) or (self.coeficiente_a < 0 and self.coeficiente_b < 0):
             nombreConica = "Elipse"
-            justificacion = "Los coeficientes A (" + str(self.coeficiente_a) + ") y C (" + str(self.coeficiente_c) + ") tienen el mismo signo y son distintos entre si."
-            
+            justificacion = "Los coeficientes A (" + str(self.coeficiente_a) + ") y B (" + str(self.coeficiente_b) + ") tienen el mismo signo y son distintos entre si."
+
         else:
             nombreConica = "Hiperbola"
-            justificacion = "Los coeficientes A (" + str(self.coeficiente_a) + ") y C (" + str(self.coeficiente_c) + ") tienen signos opuestos."
-            
+            justificacion = "Los coeficientes A (" + str(self.coeficiente_a) + ") y B (" + str(self.coeficiente_b) + ") tienen signos opuestos."
+
         return nombreConica, justificacion
 
     def ejecutar_clasificacion(self):
         self.comprobar_caso_degenerado()
-        
+
         nombreConica, justificacion = self.determinar_clasificacion_y_justificacion()
-        
+
         self.coeficientes['nombreConica'] = nombreConica
         self.coeficientes['justificacion'] = justificacion
 
         resultadoFinal = nombreConica + ": " + justificacion
 
         return resultadoFinal
-
